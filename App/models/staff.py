d16568b266e39d4b106d991103dda88f7d21ec68
LYS312 <lyssilis312@gmail.com>
2022-10-02 21:48:59 +0000

    rename user class

--- App/models/staff.py
+++ App/models/staff.py
@@ -1,6 +1,6 @@
 from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
-
+#test push
 class Staff(db.Model):
     staffID = db.Column(db.Integer, primary_key=True)
     username =  db.Column(db.String, nullable=False)
