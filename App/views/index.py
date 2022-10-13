da00abc3be592d0740d89a1022241aecee984b7a
LYS312 <lyssilis312@gmail.com>
2022-10-13 18:59:22 +0000

    comment off index view /<id>

--- App/views/index.py
+++ App/views/index.py
@@ -6,8 +6,8 @@ index_views = Blueprint('index_views', __name__, template_folder='../templates')
 def index_page():
     return render_template('index.html')
 
-@index_views.route('/<id>', methods=['GET'])
-def get_student_reviews():
-    return render_template('index.html')
+#@index_views.route('/<id>', methods=['GET'])
+#def get_student_reviews():
+#    return render_template('index.html')
 
 
