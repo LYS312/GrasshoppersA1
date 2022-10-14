0cb2b9d392ba5a550ad38cf75e820ed3880d5317
Michelle <michelle.01.liu@outlook.com>
2022-10-14 01:38:26 +0000

    edit db relp in student model

--- App/models/student.py
+++ App/models/student.py
@@ -4,7 +4,7 @@ from App.models.review import db, Review
 class Student (db.Model):
     studentID= db.Column (db.Integer, primary_key=True)
     name = db.Column (db.String(120), nullable=False)
-    reviews= db.relationship('Review', backref="student", lazy="dynamic", cascade="all, delete-orphan")
+    reviews= db.relationship('Review', backref="student", lazy=True, cascade="all, delete-orphan")
 
     #reviews= db.relationship('Review', lazy=True, cascade="all, delete-orphan",back_populates="student")
     #reviews= db.relationship('Review')
